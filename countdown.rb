973ef83a7d8cfac87c84ff5c46efd2bdb1e1139c
Casey Zimmer <czimmer@ramapo.edu>
2018-09-12 23:47:10 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,24 @@
 #write your code here
 
-def countdown
+def countdown(input)
+  
+  while input != 0 do
+    
+    puts "#{input} SECOND(S)!"
+    input -= 1
+  end 
+  
+  return "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep(input)
+  while input != 0 do
+    
+    puts "#{input} SECOND(S)!"
+    input -= 1
+    sleep(1)
+  end 
+  
+  return "HAPPY NEW YEAR!"
 end
